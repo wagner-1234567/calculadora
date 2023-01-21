f81ca41341d6357d0fc593dc552f75861cd2e5fc
wagner-1234567 <wagner-wnp@hotmail.com>
2023-01-21 10:51:47 -0300

    Corrigido botao +/-

--- App.js
+++ App.js
@@ -52,7 +52,8 @@ export default function App() {
         calculator()
         return
       case '+/-':
-        
+        setCurrentNumber(currentNumber * -1)        
+                
         return
     }
 
